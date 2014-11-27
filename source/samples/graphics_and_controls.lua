837afa4aec12aba1d2d733e6ba08313427e25c66
Rinnegatamante <rinnegatamante@gmail.com>
2014-11-27 23:44:27 +0100

    Added Graphics & Controls module sample.
    Added Graphics & Controls module sample.

---
+++ source/samples/graphics_and_controls.lua
@@ -0,0 +1,20 @@
+red = Color.new(255,0,0)
+blue = Color.new(0,0,255)
+green = Color.new(0,255,0)
+while true do
+Screen.waitVblankStart()
+Controls.init()
+pad = Controls.read()
+Screen.refresh()
+Screen.clear(TOP_SCREEN)
+Screen.fillRect(5,150,5,20,green,TOP_SCREEN)
+if (Controls.check(pad,KEY_A)) then
+Screen.debugPrint(6,6,"Button A pressed",blue,TOP_SCREEN)
+else
+Screen.debugPrint(6,6,"Button A not pressed",blue,TOP_SCREEN)
+end
+Screen.debugPrint(0,0,"LPP 3DS Test Script",red,BOTTOM_SCREEN)
+Screen.debugPrint(0,15,"Screen & Color Modules - OK",red,BOTTOM_SCREEN)
+Screen.debugPrint(0,30,"Controls Module - OK",red,BOTTOM_SCREEN)
+Screen.flip()
+end
