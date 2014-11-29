69ee97ae9b764109d6ff8351a5475dfda30831de
Rinnegatamante <rinnegatamante@gmail.com>
2014-11-29 12:07:41 +0100

    Added math library sample.
    Added math library sample.

---
+++ source/samples/math.lua
@@ -0,0 +1,17 @@
+red = Color.new(255,0,0)
+blue = Color.new(0,0,255)
+green = Color.new(0,255,0)
+while true do
+Screen.waitVblankStart()
+Controls.init()
+pad = Controls.read()
+Screen.refresh()
+Screen.clear(TOP_SCREEN)
+if (Controls.check(pad,KEY_A)) then
+System.exit()
+end
+Screen.debugPrint(0,math.random(5,50),"Test math.random",red,TOP_SCREEN)
+Screen.debugPrint(0,0,"LPP 3DS Test Script",red,BOTTOM_SCREEN)
+Screen.debugPrint(0,15,"math Library - OK",red,BOTTOM_SCREEN)
+Screen.flip()
+end
