5c69513775e58f1cc1d70ec317bd7a4379bce78b
Rinnegatamante <rinnegatamante@gmail.com>
2014-11-30 12:32:24 +0100

    Added Timer sample.
    Added Timer sample.

---
+++ source/samples/timer.lua
@@ -0,0 +1,14 @@
+red = Color.new(255,0,0)
+timer = Timer.new()
+while true do
+Screen.waitVblankStart()
+Controls.init()
+pad = Controls.read()
+Screen.refresh()
+Screen.clear(BOTTOM_SCREEN)
+if (Controls.check(pad,KEY_A)) then
+System.exit()
+end
+Screen.debugPrint(0,30,"You lost " .. math.ceil(Timer.getTime(timer)/1000) .. " seconds of your life.",red,BOTTOM_SCREEN)
+Screen.flip()
+end
