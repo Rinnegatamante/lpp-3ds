15b2f1e41ecc70a6a373f80894f404c7db83ec4c
Rinnegatamante <rinnegatamante@gmail.com>
2015-02-18 14:53:26 +0100

    Added samplerate config to converter.
    Added samplerate config to converter.

--- JPGV/vid2jpgv_LQ.bat
+++ JPGV/vid2jpgv_LQ.bat
@@ -2,8 +2,9 @@ REM Change input.mp4 with your own file
 REM Change 240x400 with your own resolution (NOTE: HEIGHTxWIDTH !!!)
 REM Change -r 30 with your own framerate
 REM Change -ac 2 with your own audiochannel
+REM Change -ar 44100 with your own audio samplerate
 ffmpeg -i "input.mp4" -r 30 -vf "transpose=1" -s 240x400 "temp\output%%1d.jpg"
-ffmpeg.exe -i "input.mp4" -acodec pcm_s16le -ac 2 temp\audio.wav"
+ffmpeg.exe -i "input.mp4" -acodec pcm_s16le -ac 2 -ar 44100 temp\audio.wav"
 REM Change 30 with your own framerate
 jpgv_encoder.exe 30
 del /q ".\temp\*.*"
