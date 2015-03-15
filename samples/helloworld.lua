cfea5bccf4e7f13379e13f932bb749812c4d9562
Rinnegatamante <rinnegatamante@gmail.com>
2015-03-15 01:23:19 +0100

    Code correctly indented.
    Code correctly indented.

--- samples/helloworld.lua
+++ samples/helloworld.lua
@@ -1,6 +1,6 @@
 while true do
-Screen.waitVblankStart()
-Screen.refresh()
-Screen.debugPrint(0,0,"Hello World",Color.new(255,255,255),TOP_SCREEN)
-Screen.flip()
+	Screen.waitVblankStart()
+	Screen.refresh()
+	Screen.debugPrint(0,0,"Hello World",Color.new(255,255,255),TOP_SCREEN)
+	Screen.flip()
 end
