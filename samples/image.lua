d09441d75332c34f1b947a0179d1685587c591f9
Rinnegatamante <rinnegatamante@gmail.com>
2015-03-15 01:18:27 +0100

    Updated Image sample to newest syntax.
    Updated Image sample to newest syntax.

--- samples/image.lua
+++ samples/image.lua
@@ -2,7 +2,7 @@ white = Color.new(255,255,255)
 black = Color.new(0,0,0)
 oldpad = Controls.read()
 bitmap2 = Screen.loadImage("/file.bmp")
-bitmap = Screen.createImage(1,1)
+bitmap = Screen.createImage(1,1,black)
 Screen.flipImage(bitmap2,bitmap)
 Screen.fillRect(50,150,1,15,white,bitmap2)
 Screen.debugPrint(51,3,"Image...OK!",black,bitmap2)
@@ -16,7 +16,7 @@ while true do
 	end
 	if (Controls.check(pad,KEY_B)) and not (Controls.check(oldpad,KEY_B)) then
 		Screen.saveBitmap(bitmap2,"/bitmap.bmp")
-		System.takeScreenshot("/screenshot.bmp")
+		System.takeScreenshot("/screenshot.bmp",false)
 	end
 	Screen.drawImage(100,127,bitmap2,TOP_SCREEN)
 	Screen.drawImage(60,0,bitmap,BOTTOM_SCREEN)
