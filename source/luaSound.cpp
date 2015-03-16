2fba824d4c8f20e3a557737d95dd8cb2a1f7ad8a
Rinnegatamante <rinnegatamante@gmail.com>
2015-03-16 21:24:18 +0100

    Added openOgg concept (Still not work).
    Added openOgg concept (Still not work).

--- source/luaSound.cpp
+++ source/luaSound.cpp
@@ -65,48 +65,93 @@ u8 encoding;
 
 int STREAM_MAX_ALLOC = 524288;
 
-/* THIS IS A TEST FUNCTION TO TEST LIBOGG AND LIBVORBIS FUNCTIONALLITIES
- IT OPENS A OGG MUSIC FILE AND EXTRACT DECODED AUDIOBUFFER IN A BIN FILE
- DECODED BUFFER IS IN S_PCM16_LE CODEC
-*/
-static int lua_playogg(lua_State *L) 
+// Don't know why but i get a mute buffer (all bytes 0x0) if someone knows how to fix
+// please make a pull request on this repository
+static int lua_openogg(lua_State *L) 
 {	
-	char pcmout[4096];
-	const char *file_tbo = luaL_checkstring(L, 1);
+    int argc = lua_gettop(L);
+	if ((argc != 1) && (argc != 2)) return luaL_error(L, "wrong number of arguments");
+	const char *file_tbo = luaL_checkstring(L, 1); //Filename
+	
+	// Future streaming support
+	u32 mem_size = 0;
+	if (argc == 2) mem_size = luaL_checkinteger(L, 2);
+	
+	char pcmout[4096]; //Does it can be incresed or it crashes if increased?
+	
+	// Using stdio instead of FS
 	char myFile[512];
 	strcpy(myFile,"sdmc:");
 	strcat(myFile,file_tbo);
 	sdmcInit();
+	
+	// Initializing libogg and vorbisfile
 	int eof=0;
 	static OggVorbis_File vf;
 	static int current_section;
+	
+	// Opening a valid OGG music file
 	FILE* fp = fopen(myFile,"rb");
-	FILE* fout = fopen("sdmc:/pcm.bin","wb");
 	if(ov_open(fp, &vf, NULL, 0) != 0)
 	{
 		fclose(fp);
 		return luaL_error(L, "corrupt OGG file.");
 	}
-	while(!eof){
-    long ret=ov_read(&vf,pcmout,sizeof(pcmout),0,2,1,&current_section);
-    if (ret == 0) {
-      /* EOF */
-      eof=1;
-    } else if (ret < 0) {
-      if(ret==OV_EBADLINK){
-        return luaL_error(L, "corrupt bitstream section.");
-      }
-
-      /* some other error in the stream.  Not a problem, just reporting it in
-         case we (the app) cares.  In this case, we don't. */
-    } else {
-      /* we don't bother dealing with sample rate changes, etc, but
-         you'll have to*/
-      fwrite(pcmout,1,ret,fout);
-    }
-  }
+	
+	// Allocating music info
+	wav *wav_file = (wav*)malloc(sizeof(wav));
+	vorbis_info* my_info = ov_info(&vf,-1);
+	wav_file->samplerate = my_info->rate;
+	wav_file->big_endian = false;
+	wav_file->encoding = CSND_ENCODING_PCM16;
+	wav_file->size = ov_time_total(&vf,-1) * 2 * my_info->rate;
+	wav_file->audiobuf = (u8*)linearAlloc(wav_file->size);
+	wav_file->audiobuf2 = NULL;
+	wav_file->mem_size = mem_size;
+	wav_file->startRead = 0;
+	strcpy(wav_file->author,"");
+	strcpy(wav_file->title,"");
+	wav_file->isPlaying = false;
+	wav_file->bytepersample = 2;
+	
+	// Decoding OGG buffer
+	u32 i=0;
+	if (my_info->channels == 1){ //Mono buffer
+		while(!eof){
+			long ret=ov_read(&vf,pcmout,sizeof(pcmout),0,2,1,&current_section);
+			if (ret == 0) {
+			
+				// EOF
+				eof=1;
+				
+			} else if (ret < 0) {
+			
+				// Error handling
+				if(ret==OV_EBADLINK){
+					return luaL_error(L, "corrupt bitstream section.");
+				}
+				
+			} else {
+			
+				// Copying decoded block to PCM16 audiobuffer
+				int z;
+				for (z=0;z++;z<ret){
+					wav_file->audiobuf[i+z] = pcmout[z];
+				}
+				i = i + ret;
+				
+			}
+		}
+	}
+	
+	// Deallocate OGG decoder resources and close file
+	ov_clear(&vf);
 	sdmcExit();
-	return 0;
+	
+	// Push wav struct offset to LUA stack
+	lua_pushinteger(L,(u32)wav_file);
+	
+	return 1;
 }
 
 static int lua_openwav(lua_State *L)
@@ -228,8 +273,6 @@ static int lua_openwav(lua_State *L)
 					FSFILE_Read(fileHandle, &bytesRead, pos+4, wav_file->audiobuf, size-(pos+4));
 					size = size-(pos+4);
 				}
-				CSND_setchannel_playbackstate(0x08, 1);
-				CSND_sharedmemtype0_cmdupdatestate(0);
 				wav_file->audiobuf2 = NULL;
 				wav_file->size = size;
 				wav_file->startRead = 0;
@@ -911,7 +954,7 @@ int argc = lua_gettop(L);
 
 //Register our Sound Functions
 static const luaL_Reg Sound_functions[] = {
-  {"playOgg",				lua_playogg},
+  {"openOgg",				lua_openogg},
   {"openWav",				lua_openwav},
   {"openAiff",				lua_openaiff},
   {"close",					lua_closeWav},
