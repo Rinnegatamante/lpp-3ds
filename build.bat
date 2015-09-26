f23d96f0834e7ee9c1a4a18194f92ba827b9d29a
Rinnegatamante <rinnegatamante@gmail.com>
2015-09-26 12:21:07 +0200

    Added libctru compilation to buildscript.
    Added libctru compilation to buildscript.

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+cd libctru
+make
+cd ..
 make
 arm-none-eabi-strip lpp-3ds.elf
 makerom2 -f cci -o lpp-3ds.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf lpp-3ds.elf -icon icon.bin -banner banner.bin
