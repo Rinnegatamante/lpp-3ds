c8b4713b5bbc510654a070fd2b3c3235e319e6fc
Rinnegatamante <rinnegatamante@gmail.com>
2015-11-30 15:48:14 +0100

    Deprecated BMPV module.
    Deprecated BMPV module.

--- source/luaVideo.cpp
+++ source/luaVideo.cpp
@@ -51,30 +51,7 @@
 int MAX_RAM_ALLOCATION = 524288;
 int MAX_RAM_ALLOCATION_44100 = 524288*2;
 u8* tmp_buf = NULL;
-
-struct BMPV{
-	u32 magic;
-	Handle sourceFile;
-	u32 currentFrame;
-	u32 width;
-	u32 height;
-	u32 framerate;
-	u64 tot_frame;
-	u64 tick;
-	u32 audio_size;
-	u16 bytepersample;
-	u16 audiotype;
-	u32 samplerate;
-	u8* audiobuf;
-	u8* audiobuf2;
-	u8* framebuf;
-	int ch1;
-	int ch2;
-	bool isPlaying;
-	int loop;
-	u32 mem_size;
-	u32 moltiplier;
-};
+extern bool audioChannels[24];
 
 struct JPGV{
 	u32 magic;
@@ -580,110 +557,6 @@ static int lua_startJPGV(lua_State *L){
 	return 0;
 }
 
-
-static int lua_loadBMPV(lua_State *L)
-{
-    int argc = lua_gettop(L);
-	#ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	const char *file_tbo = luaL_checkstring(L, 1);
-	Handle fileHandle;
-	FS_Archive sdmcArchive=(FS_Archive){ARCHIVE_SDMC, (FS_Path){PATH_EMPTY, 1, (u8*)""}};
-	FS_Path filePath=fsMakePath(PATH_ASCII, file_tbo);
-	Result ret=FSUSER_OpenFileDirectly( &fileHandle, sdmcArchive, filePath, FS_OPEN_READ, 0x00000000);
-	#ifndef SKIP_ERROR_HANDLING
-		if(ret) return luaL_error(L, "error opening file");
-	#endif
-	u32 magic,frame_size,bytesRead;
-	u64 size;
-	FSFILE_GetSize(fileHandle, &size);
-	FSFILE_Read(fileHandle, &bytesRead, 0, &magic, 4);
-	if (magic == 0x56504D42){
-	BMPV* BMPV_file = (BMPV*)malloc(sizeof(BMPV));
-	FSFILE_Read(fileHandle, &bytesRead, 4, &(BMPV_file->framerate), 4);
-	FSFILE_Read(fileHandle, &bytesRead, 8, &(BMPV_file->width), 4);
-	FSFILE_Read(fileHandle, &bytesRead, 12,&(BMPV_file->height), 4);
-	FSFILE_Read(fileHandle, &bytesRead, 16,&(BMPV_file->audiotype), 2);
-	FSFILE_Read(fileHandle, &bytesRead, 18,&(BMPV_file->bytepersample), 2);
-	FSFILE_Read(fileHandle, &bytesRead, 20,&(BMPV_file->samplerate), 4);
-	FSFILE_Read(fileHandle, &bytesRead, 24,&(BMPV_file->audio_size), 4);
-	BMPV_file->isPlaying = false;
-	BMPV_file->currentFrame = 0;
-	BMPV_file->sourceFile = fileHandle;
-	BMPV_file->tick = 0;
-	BMPV_file->audiobuf = NULL;
-	BMPV_file->audiobuf2 = NULL;
-	frame_size = BMPV_file->width*BMPV_file->height*3;
-	u8* framebuf = (u8*)(malloc(frame_size));
-	BMPV_file->framebuf = framebuf;
-	int tot_frame = (size-28-BMPV_file->audio_size)/frame_size;
-	BMPV_file->mem_size = BMPV_file->audio_size;
-	BMPV_file->tot_frame = tot_frame;
-	BMPV_file->magic = 0x4C424D56;
-	lua_pushinteger(L, (u32)BMPV_file);
-	}
-	return 1;
-}
-
-static int lua_startBMPV(lua_State *L){
-int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if ((argc != 3) && (argc != 4)) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	int loop = luaL_checkinteger(L, 2);
-	int ch1 = luaL_checkinteger(L, 3);
-	if (argc == 4){
-	int ch2 = luaL_checkinteger(L, 4);
-	src->ch2 = ch2;
-	}
-	src->loop = loop;
-	src->isPlaying = true;
-	src->ch1 = ch1;
-	src->currentFrame = 0;
-	u32 bytesRead;
-	if (src->samplerate != 0 && src->audio_size != 0){
-		while(src->mem_size > MAX_RAM_ALLOCATION){
-			src->mem_size = src->mem_size / 2;
-		}
-		if (src->audiotype == 1){
-			FSFILE_Read(src->sourceFile, &bytesRead, 28, src->audiobuf, src->mem_size);
-			My_CSND_playsound(ch1, SOUND_LINEAR_INTERP | SOUND_FORMAT_16BIT | SOUND_REPEAT, src->samplerate, (u32*)src->audiobuf, (u32*)src->audiobuf, src->mem_size, 1.0, 2.0);
-		}else{
-			u8* audiobuf = (u8*)linearAlloc(src->mem_size);
-			FSFILE_Read(src->sourceFile, &bytesRead, 28, audiobuf, src->mem_size);
-			src->audiobuf = (u8*)linearAlloc(src->mem_size/2);
-			src->audiobuf2 = (u8*)linearAlloc(src->mem_size/2);
-			u32 off=0;
-			u32 i=0;
-			u16 z;
-			while (i < (src->mem_size)){
-				z=0;
-				while (z < (src->bytepersample/2)){
-					src->audiobuf[off+z] = audiobuf[i+z];
-					src->audiobuf2[off+z] = audiobuf[i+z+(src->bytepersample/2)];
-					z++;
-				}
-				i=i+src->bytepersample;
-				off=off+(src->bytepersample/2);
-			}
-			linearFree(audiobuf);
-			My_CSND_playsound(src->ch1, SOUND_LINEAR_INTERP | SOUND_FORMAT_16BIT | SOUND_REPEAT, src->samplerate, (u32*)src->audiobuf, (u32*)src->audiobuf, src->mem_size/2, 1.0, 1.0);
-			My_CSND_playsound(src->ch2, SOUND_LINEAR_INTERP | SOUND_FORMAT_16BIT | SOUND_REPEAT, src->samplerate, (u32*)src->audiobuf2, (u32*)src->audiobuf2, src->mem_size/2, 1.0, -1.0);
-		}
-		CSND_SetPlayState(ch1, 1);
-		if (src->audiotype == 2) CSND_SetPlayState(src->ch2, 1);
-		CSND_UpdateInfo(0);
-		src->tick = osGetTime();
-		src->moltiplier = 1;
-	}else src->tick = osGetTime();
-	return 0;
-}
-
 void draw3DJPGV(int x,int y,JPGV* src,int screen,bool use3D){
 	if (src->isPlaying){
 		if (src->currentFrame >= (src->tot_frame - 10)){
@@ -858,151 +731,6 @@ static int lua_drawJPGV(lua_State *L){
 	return 0;
 }
 
-static int lua_drawBMPV(lua_State *L){
-int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-    if ((argc != 4) && (argc != 5)) return luaL_error(L, "wrong number of arguments");
-	#endif
-	int x = luaL_checkinteger(L, 1);
-	int y = luaL_checkinteger(L, 2);
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 3);
-	u32 bytesRead;
-	int screen = luaL_checkinteger(L, 4);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-		if ((x < 0) || (y < 0)) return luaL_error(L,"out of bounds");
-		if ((screen <= 1) && (y+src->height > 240)) return luaL_error(L,"out of framebuffer bounds");
-		if ((screen == 0) && (x+src->width > 400)) return luaL_error(L,"out of framebuffer bounds");
-		if ((screen == 1) && (x+src->width > 320)) return luaL_error(L,"out of framebuffer bounds");
-	#endif
-	int side = 0;
-	if (argc == 5) side = luaL_checkinteger(L,5);
-	if (src->isPlaying){
-		if (src->currentFrame >= src->tot_frame){
-			if (src->loop == 1){
-				src->currentFrame = 0;
-					src->moltiplier = 1;
-				src->tick = osGetTime();
-			}else{
-				src->isPlaying = false;
-				src->moltiplier = 1;
-				CSND_SetPlayState(src->ch1, 0);
-				if (src->audiobuf2 != NULL) CSND_SetPlayState(src->ch2, 0);
-				CSND_UpdateInfo(0);
-			}
-			if (src->audiobuf2 == NULL) FSFILE_Read(src->sourceFile, &bytesRead, 28, src->audiobuf, src->mem_size);
-			else{
-				u8* tmp_buffer = (u8*)linearAlloc(src->mem_size);
-				FSFILE_Read(src->sourceFile, &bytesRead, 28, tmp_buffer, src->mem_size);
-				u32 size_tbp = src->mem_size;
-				u32 off=0;
-				u32 i=0;
-				u16 z;
-				while (i < size_tbp){
-					z=0;
-					while (z < (src->bytepersample/2)){
-						src->audiobuf[off+z] = tmp_buffer[i+z];
-						src->audiobuf2[off+z] = tmp_buffer[i+z+(src->bytepersample/2)];
-						z++;
-					}
-					z=0;
-					i=i+src->bytepersample;
-					off=off+(src->bytepersample/2);
-				}
-			linearFree(tmp_buffer);
-			}
-		}else{
-			if (((src->samplerate * src->bytepersample * ((osGetTime() - src->tick) / 1000)) > ((src->mem_size / 2) * src->moltiplier)) && (src->isPlaying)){
-			if ((src->moltiplier % 2) == 1){
-			//Update and flush first half-buffer
-			if (src->audiobuf2 == NULL){
-				FSFILE_Read(src->sourceFile, &bytesRead, 28+(((src->mem_size)/2)*(src->moltiplier + 1)), src->audiobuf, (src->mem_size)/2);
-				if (bytesRead != ((src->mem_size)/2)){
-				FSFILE_Read(src->sourceFile, &bytesRead, 28, src->audiobuf, (src->mem_size)/2);
-				src->moltiplier = src->moltiplier + 1;
-				}
-				src->moltiplier = src->moltiplier + 1;
-			}else{
-				u8* tmp_buffer = (u8*)linearAlloc((src->mem_size)/2);
-				FSFILE_Read(src->sourceFile, &bytesRead, 28+(src->mem_size/2)*(src->moltiplier + 1), tmp_buffer, (src->mem_size)/2);
-				if (bytesRead != ((src->mem_size)/2)){
-				FSFILE_Read(src->sourceFile, &bytesRead, 28, tmp_buffer, (src->mem_size)/2);
-				src->moltiplier = src->moltiplier + 1;
-				}
-				src->moltiplier = src->moltiplier + 1;
-				u32 size_tbp = (src->mem_size)/2;
-				u32 off=0;
-				u32 i=0;
-				u16 z;
-				while (i < size_tbp){
-					z=0;
-					while (z < (src->bytepersample/2)){
-						src->audiobuf[off+z] = tmp_buffer[i+z];
-						src->audiobuf2[off+z] = tmp_buffer[i+z+(src->bytepersample/2)];
-						z++;
-					}
-					i=i+src->bytepersample;
-					off=off+(src->bytepersample/2);
-				}
-				linearFree(tmp_buffer);
-			}
-		}else{
-			u32 bytesRead;
-			//Update and flush second half-buffer
-			if (src->audiobuf2 == NULL){
-					FSFILE_Read(src->sourceFile, &bytesRead, 28+(((src->mem_size)/2)*(src->moltiplier + 1)), src->audiobuf+((src->mem_size)/2), (src->mem_size)/2);
-					src->moltiplier = src->moltiplier + 1;
-			}else{
-				u8* tmp_buffer = (u8*)linearAlloc((src->mem_size)/2);
-				FSFILE_Read(src->sourceFile, &bytesRead, 28+(src->mem_size/2)*(src->moltiplier + 1), tmp_buffer, (src->mem_size)/2);
-				src->moltiplier = src->moltiplier + 1;
-				u32 size_tbp = (src->mem_size)/2;
-				u32 off=0;
-				u32 i=0;
-				u16 z;
-				while (i < size_tbp){
-					z=0;
-					while (z < (src->bytepersample/2)){
-						src->audiobuf[(src->mem_size)/4+off+z] = tmp_buffer[i+z];
-						src->audiobuf2[(src->mem_size)/4+off+z] = tmp_buffer[i+z+(src->bytepersample/2)];
-						z++;
-					}
-				i=i+src->bytepersample;
-				off=off+(src->bytepersample/2);
-				}
-				linearFree(tmp_buffer);
-			}
-		}
-		}
-			src->currentFrame =((osGetTime() - src->tick) * src->framerate / 1000);
-			Bitmap bitmap;
-			bitmap.width = src->width;
-			bitmap.height = src->height;
-			bitmap.bitperpixel = 24;
-			u32 frame_size = src->width * src->height * 3;
-			u32 bytesRead;
-			FSFILE_Read(src->sourceFile, &bytesRead, 28+src->audio_size+(src->currentFrame*frame_size), src->framebuf, frame_size);
-			bitmap.pixels = src->framebuf;
-			if (screen > 1) PrintImageBitmap(x,y,&bitmap,screen);
-			else PrintScreenBitmap(x,y,&bitmap,screen,side);
-		}
-	}else{
-		if (src->tick != 0){
-			Bitmap bitmap;
-			bitmap.width = src->width;
-			bitmap.height = src->height;
-			bitmap.bitperpixel = 24;
-			u32 frame_size = src->width * src->height * 3;
-			u32 bytesRead;
-			FSFILE_Read(src->sourceFile, &bytesRead, 28+src->audio_size+(src->currentFrame*frame_size), src->framebuf, frame_size);
-			bitmap.pixels = src->framebuf;
-			if (screen > 1) PrintImageBitmap(x,y,&bitmap,screen);
-			else PrintScreenBitmap(x,y,&bitmap,screen,side);
-		}
-	}
-	return 0;
-}
-
 void draw3DJPGVFrame(u16 x,u16 y,JPGV* src,u32 frame_index,u8 screen,bool is3D){
 	u32 bytesRead;
 	u64 offset;
@@ -1080,53 +808,6 @@ int argc = lua_gettop(L);
 	return 0;
 }
 
-static int lua_BMPVshowFrame(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if ((argc != 5) && (argc != 6)) return luaL_error(L, "wrong number of arguments");
-	#endif
-	u16 x = luaL_checkinteger(L, 1);
-	u16 y = luaL_checkinteger(L, 2);
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 3);
-	u32 frame_index = luaL_checkinteger(L, 4);
-	u8 screen = luaL_checkinteger(L, 5);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-		if ((x < 0) || (y < 0)) return luaL_error(L,"out of bounds");
-		if ((screen <= 1) && (y+src->height > 240)) return luaL_error(L,"out of framebuffer bounds");
-		if ((screen == 0) && (x+src->width > 400)) return luaL_error(L,"out of framebuffer bounds");
-		if ((screen == 1) && (x+src->width > 320)) return luaL_error(L,"out of framebuffer bounds");
-		if (frame_index > src->tot_frame) return luaL_error(L,"out of video file bounds");
-	#endif
-	int side = 0;
-	if (argc == 6) side = luaL_checkinteger(L,6);
-	u32 bytesRead;
-	Bitmap bitmap;
-	bitmap.width = src->width;
-	bitmap.height = src->height;
-	bitmap.bitperpixel = 24;
-	u32 frame_size = src->width * src->height * 3;
-	u8* tmp_buffer;
-	FSFILE_Read(src->sourceFile, &bytesRead, 28+src->audio_size+(src->currentFrame*frame_size), tmp_buffer, frame_size);
-	bitmap.pixels = tmp_buffer;
-	PrintScreenBitmap(x,y,&bitmap,screen,side);
-	free(bitmap.pixels);
-	return 0;
-}
-
-static int lua_getFPS(lua_State *L){
-	int argc = lua_gettop(L);
-	#ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	lua_pushinteger(L, src->framerate);
-	return 1;
-}
-
 static int lua_getFPS2(lua_State *L){
 	int argc = lua_gettop(L);
 	#ifndef SKIP_ERROR_HANDLING
@@ -1137,19 +818,6 @@ static int lua_getFPS2(lua_State *L){
 		if (src->magic != 0x4C4A5056) return luaL_error(L, "attempt to access wrong memory block type");
 	#endif
 	lua_pushinteger(L, src->framerate);
-	return 1;
-}
-
-static int lua_getCF(lua_State *L){
-	int argc = lua_gettop(L);
-	#ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	lua_pushinteger(L, src->currentFrame);
 	return 1;
 }
 
@@ -1166,19 +834,6 @@ static int lua_getCF2(lua_State *L){
 	return 1;
 }
 
-static int lua_getSize(lua_State *L){
-	int argc = lua_gettop(L);
-	#ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	lua_pushinteger(L, src->tot_frame);
-	return 1;
-}
-
 static int lua_getSize2(lua_State *L){
 	int argc = lua_gettop(L);
 	#ifndef SKIP_ERROR_HANDLING
@@ -1189,19 +844,6 @@ static int lua_getSize2(lua_State *L){
 		if (src->magic != 0x4C4A5056) return luaL_error(L, "attempt to access wrong memory block type");
 	#endif
 	lua_pushinteger(L, src->tot_frame);
-	return 1;
-}
-
-static int lua_getSrate(lua_State *L){
-	int argc = lua_gettop(L);
-	#ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	lua_pushinteger(L, src->samplerate);
 	return 1;
 }
 
@@ -1218,19 +860,6 @@ static int lua_getSrate2(lua_State *L){
 	return 1;
 }
 
-static int lua_isPlaying(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	lua_pushboolean(L, src->isPlaying);
-	return 1;
-}
-
 static int lua_isPlaying2(lua_State *L){
 	int argc = lua_gettop(L);
     #ifndef SKIP_ERROR_HANDLING
@@ -1242,28 +871,6 @@ static int lua_isPlaying2(lua_State *L){
 	#endif
 	lua_pushboolean(L, src->isPlaying);
 	return 1;
-}
-
-static int lua_unloadBMPV(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	if (src->samplerate != 0 && src->audio_size != 0){
-	linearFree(src->audiobuf);
-	if (src->audiotype == 2){
-		linearFree(src->audiobuf2);
-	}
-	}
-	FSFILE_Close(src->sourceFile);
-	svcCloseHandle(src->sourceFile);
-	free(src->framebuf);
-	free(src);
-	return 0;
 }
 
 static int lua_unloadJPGV(lua_State *L){
@@ -1297,27 +904,6 @@ static int lua_unloadJPGV(lua_State *L){
 	return 0;
 }
 
-static int lua_stopBMPV(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	src->isPlaying = false;
-	src->currentFrame = 0;
-	if (src->samplerate != 0 && src->audio_size != 0){
-	CSND_SetPlayState(src->ch1, 0);
-	if (src->audiotype == 2){
-	CSND_SetPlayState(src->ch2, 0);
-	}
-	CSND_UpdateInfo(0);
-	}
-	return 0;
-}
-
 static int lua_stopJPGV(lua_State *L){
 	int argc = lua_gettop(L);
     #ifndef SKIP_ERROR_HANDLING
@@ -1329,27 +915,6 @@ static int lua_stopJPGV(lua_State *L){
 	#endif
 	src->isPlaying = false;
 	src->currentFrame = 0;
-	if (src->samplerate != 0 && src->audio_size != 0){
-	CSND_SetPlayState(src->ch1, 0);
-	if (src->audiotype == 2){
-	CSND_SetPlayState(src->ch2, 0);
-	}
-	CSND_UpdateInfo(0);
-	}
-	return 0;
-}
-
-static int lua_pauseBMPV(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	src->isPlaying = false;
-	src->tick = (osGetTime() - src->tick);
 	if (src->samplerate != 0 && src->audio_size != 0){
 	CSND_SetPlayState(src->ch1, 0);
 	if (src->audiotype == 2){
@@ -1381,27 +946,6 @@ static int lua_pauseJPGV(lua_State *L){
 	return 0;
 }
 
-static int lua_resumeBMPV(lua_State *L){
-	int argc = lua_gettop(L);
-    #ifndef SKIP_ERROR_HANDLING
-		if (argc != 1) return luaL_error(L, "wrong number of arguments");
-	#endif
-	BMPV* src = (BMPV*)luaL_checkinteger(L, 1);
-	#ifndef SKIP_ERROR_HANDLING
-		if (src->magic != 0x4C424D56) return luaL_error(L, "attempt to access wrong memory block type");
-	#endif
-	src->isPlaying = true;
-	src->tick = (osGetTime() - src->tick);
-	if (src->samplerate != 0 && src->audio_size != 0){
-	CSND_SetPlayState(src->ch1, 1);
-	if (src->audiotype == 2){
-	CSND_SetPlayState(src->ch2, 1);
-	}
-	CSND_UpdateInfo(0);
-	}
-	return 0;
-}
-
 static int lua_resumeJPGV(lua_State *L){
 	int argc = lua_gettop(L);
     #ifndef SKIP_ERROR_HANDLING
@@ -1423,24 +967,6 @@ static int lua_resumeJPGV(lua_State *L){
 	return 0;
 }
 
-//Register our BMPV Functions
-static const luaL_Reg BMPV_functions[] = {
-  {"load",				lua_loadBMPV},
-  {"start",				lua_startBMPV},
-  {"draw",				lua_drawBMPV},
-  {"unload",			lua_unloadBMPV},
-  {"getFPS",			lua_getFPS},
-  {"getFrame",			lua_getCF},
-  {"showFrame",			lua_BMPVshowFrame},
-  {"getSize",			lua_getSize},
-  {"getSrate",			lua_getSrate},
-  {"isPlaying",			lua_isPlaying},
-  {"stop",				lua_stopBMPV},
-  {"resume",			lua_resumeBMPV},
-  {"pause",				lua_pauseBMPV},
-  {0, 0}
-};
-
 //Register our JPGV Functions
 static const luaL_Reg JPGV_functions[] = {
   {"load",				lua_loadJPGV},
@@ -1460,9 +986,6 @@ static const luaL_Reg JPGV_functions[] = {
 };
 
 void luaVideo_init(lua_State *L) {
-	lua_newtable(L);
-	luaL_setfuncs(L, BMPV_functions, 0);
-	lua_setglobal(L, "BMPV");
 	lua_newtable(L);
 	luaL_setfuncs(L, JPGV_functions, 0);
 	lua_setglobal(L, "JPGV");
