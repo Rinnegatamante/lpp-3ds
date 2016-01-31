21c7c9476d2b35d3d6f50e46177757c675fbfb40
Rinnegatamante <rinnegatamante@gmail.com>
2016-01-31 21:15:05 +0100

    Updated makerom build to latest version.
    Updated makerom build to latest version.

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ make
 cd ..
 make
 arm-none-eabi-strip lpp-3ds.elf
-makerom2 -f cci -o lpp-3ds.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf lpp-3ds.elf -icon icon.bin -banner banner.bin
+makerom -f cci -o lpp-3ds.3ds -rsf gw_workaround.rsf -target d -exefslogo -elf lpp-3ds.elf -icon icon.bin -banner banner.bin
 makerom -f cia -o lpp-3ds.cia -elf lpp-3ds.elf -rsf cia_workaround.rsf -icon icon.bin -banner banner.bin -exefslogo -target t 
